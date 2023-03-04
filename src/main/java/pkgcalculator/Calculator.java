9abd6ba110fde3a205de2b6ff6b1760703fdff30
Rakesh <rakesh.vssut2022@gmail.com>
2023-03-04 12:41:31 +0530

    Test

---
+++ src/main/java/pkgcalculator/Calculator.java
@@ -0,0 +1,28 @@
+package pkgcalculator;
+
+public class Calculator {
+	int a,b;
+	
+
+	// member variables
+	public String varHi="Hello world";
+	
+	// member funcitons
+	public String getwishes() {
+		return varHi;
+	}
+	
+	public int addition(int a,int b) {
+		return a+b;
+	}
+	public int substraction(int a,int b) {
+		return a-b;
+	}
+	public int Multiplication(int a,int b) {
+		return a*b;
+	}
+	public int division(int a,int b) {
+		return a/b;
+	}
+
+}
